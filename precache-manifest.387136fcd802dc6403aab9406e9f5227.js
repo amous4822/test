342804959c48a7d71b7f161790c0bfe8bf0c3d83
amous4822 <amous4822@gmail.com>
2019-06-26 01:36:40 +0530

    Updates

---
+++ precache-manifest.387136fcd802dc6403aab9406e9f5227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6d5b731aeed6ff3ac780635d260c73",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a19d392514ae9362cef7",
+    "url": "/test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a19d392514ae9362cef7",
+    "url": "/test/static/js/main.3151093b.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
